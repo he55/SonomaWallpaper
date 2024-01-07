8ce43ccf3a9eefd1ddc8c489c1edb0ac1c509e0c
he55 <40764272+he55@users.noreply.github.com>
2024-01-07 15:31:18 +0800

    add copyfile.bat

---
+++ src/copyfile.bat
@@ -0,0 +1,14 @@
+set OUTPUT="SonomaWallpaper"
+
+mkdir %OUTPUT%
+cd %OUTPUT%
+
+mkdir "data"
+copy /Y "..\data" "data"
+
+copy /Y "..\Microsoft.Toolkit.Win32.UI.XamlHost.Managed.dll" .
+copy /Y "..\Microsoft.Toolkit.Win32.UI.XamlHost.winmd" .
+copy /Y "..\Microsoft.Toolkit.Wpf.UI.XamlHost.dll" .
+copy /Y "..\ModernWpf.Controls.dll" .
+copy /Y "..\ModernWpf.dll" .
+copy /Y "..\SonomaWallpaper.exe" .
