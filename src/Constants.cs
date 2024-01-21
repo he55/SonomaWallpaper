9f495163dbec32ac75ecfd89d543879d64932044
he55 <40764272+he55@users.noreply.github.com>
2024-01-21 13:41:37 +0800

    2

--- src/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SonomaWallpaper
-{
-    public static class Constants
-    {
-        public const string ProjectName = nameof(SonomaWallpaper);
-        public const string Version = "1.0";
-        public const string MainWindowTitle = ProjectName + " ";
-    }
-}
