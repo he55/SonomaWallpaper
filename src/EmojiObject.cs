4c26a0f9c7585e03082390e78afa5c4b67da8a7b
he55 <40764272+he55@users.noreply.github.com>
2024-01-21 13:39:16 +0800

    1

---
+++ src/EmojiObject.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace MacWallpaper
+{
+    public class EmojiObject
+    {
+        public string cldr { get; set; }
+        public string fromVersion { get; set; }
+        public string glyph { get; set; }
+        public string group { get; set; }
+        public string[] keywords { get; set; }
+        public string[] mappedToEmoticons { get; set; }
+        public string tts { get; set; }
+        public string unicode { get; set; }
+    }
+
+    public class EmojiAsset : INotifyPropertyChanged
+    {
+        private bool _isSelected;
+        private List<AssetItem> _items;
+
+        public EmojiObject emoji { get; set; }
+        public List<AssetItem> items
+        {
+            get
+            {
+                if (_items == null)
+                    _items = AssetItemHelper.MakeItems(id);
+                return _items;
+            }
+        }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string previewImage { get; set; }
+        public string filePath { get; set; }
+        public bool isSelected
+        {
+            get => _isSelected;
+            set
+            {
+                _isSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class EmojiCategory
+    {
+        public string title { get; set; }
+        public List<EmojiAsset> assets { get; set; }
+    }
+
+    public class AssetSubitem
+    {
+        public string name { get; set; }
+        public string source { get; set; }
+    }
+
+    public class AssetItem
+    {
+        public string name { get; set; }
+        public List<AssetSubitem> subitems { get; set; }
+    }
+
+    public class AssetItemHelper
+    {
+        public static List<AssetItem> MakeItems(string path)
+        {
+            List<AssetItem> items = new List<AssetItem>();
+            string imageDir = Path.Combine(path, "3D");
+            if (Directory.Exists(imageDir))
+            {
+                items.Add(new AssetItem
+                {
+                    name = "Default",
+                    subitems = MakeSubitems(path)
+                });
+                return items;
+            }
+
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (var dir in dirs)
+            {
+                string name = Path.GetFileName(dir);
+                items.Add(new AssetItem
+                {
+                    name = name,
+                    subitems = MakeSubitems(dir)
+                });
+            }
+            return items;
+        }
+
+        static List<AssetSubitem> MakeSubitems(string path)
+        {
+            List<AssetSubitem> items = new List<AssetSubitem>();
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (var dir in dirs)
+            {
+                var files = Directory.GetFiles(dir);
+                string image = files.FirstOrDefault(x => x.EndsWith(".png", StringComparison.OrdinalIgnoreCase) || x.EndsWith(".svg", StringComparison.OrdinalIgnoreCase));
+                if (image == null)
+                    continue;
+
+                string name = Path.GetFileName(dir);
+                items.Add(new AssetSubitem
+                {
+                    name = name,
+                    source = image,
+                });
+            }
+            return items;
+        }
+    }
+}
--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Threading;
 
 namespace SonomaWallpaper
 {
@@ -17,317 +12,82 @@ namespace SonomaWallpaper
     /// </summary>
     public partial class MainWindow : Window
     {
-        System.Windows.Forms.NotifyIcon _notifyIcon;
-        Settings _settings = Settings.Load();
-        WallpaperAsset _lastSelectedAsset;
-        List<WallpaperAsset> _assets;
-        DispatcherTimer _dispatcherTimer;
-        UwpVideoWindow _uwpVideoWindow;
+        EmojiAsset _lastSelectedAsset;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            this.Title = Constants.MainWindowTitle;
-            toggleSwitch2.IsOn = Helper.CheckStartOnBoot();
-
-            if (Helper.IsWindows11OrGreater())
-            {
-                _dispatcherTimer = new DispatcherTimer();
-                _dispatcherTimer.Interval = TimeSpan.FromSeconds(5);
-                _dispatcherTimer.Tick += DispatcherTimer_Tick;
-
-                toggleSwitch1.IsOn = _settings.AutoPlay;
-
-                InitNotifyIcon();
-            }
-            else
-            {
-                toggleSwitch1.IsEnabled = false;
-            }
         }
 
-        async void PreviewVideo()
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            if (_uwpVideoWindow == null)
-                _uwpVideoWindow = new UwpVideoWindow();
-
-            _uwpVideoWindow.Source = _lastSelectedAsset.filePath;
-            _uwpVideoWindow.Play();
-            await Task.Delay(200);
-            _uwpVideoWindow.Show();
-        }
-
-        private void DispatcherTimer_Tick(object sender, EventArgs e)
-        {
-            const int seconds = 2 * 60;
-            bool val = NativeMethods.GetLastInputTickCount() > seconds * 1000;
-            if (!(_uwpVideoWindow?.IsPlaying == true) && _lastSelectedAsset.downloadState == DownloadState.downloaded && val)
-            {
-                PreviewVideo();
-            }
-        }
-
-        private async void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
-
-            FolderHelper.CreateFolder();
-            LoadData();
-            await DownloadImages(_assets);
-        }
-
-        private void Window_Closing(object sender, CancelEventArgs e)
-        {
-            if (!Helper.IsWindows11OrGreater())
-            {
-                Settings.Save();
-                return;
-            }
-
-            e.Cancel = true;
-            this.Hide();
-
-            if (_settings.FirstRun)
-            {
-                _notifyIcon.ShowBalloonTip(1000, "", I18nWpf.GetString("LHideTip"), System.Windows.Forms.ToolTipIcon.None);
-                _settings.FirstRun = false;
-            }
-
-            Settings.Save();
-        }
-
-        void ShowWindow()
-        {
-            this.WindowState = WindowState.Normal;
-            this.Show();
-            this.Activate();
-        }
-
-        void InitNotifyIcon()
-        {
-            var toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            toolStripMenuItem1.Text = I18nWpf.GetString("LMenuShow");
-            toolStripMenuItem1.Click += delegate { ShowWindow(); };
-
-            var toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
-            toolStripMenuItem2.Text = I18nWpf.GetString("LMenuExit");
-            toolStripMenuItem2.Click += toolStripMenuItem2_Click;
-
-            var contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip();
-            contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            toolStripMenuItem1,
-            toolStripMenuItem2});
-
-            _notifyIcon = new System.Windows.Forms.NotifyIcon();
-            _notifyIcon.ContextMenuStrip = contextMenuStrip1;
-            _notifyIcon.Text = Constants.ProjectName;
-            _notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath);
-            _notifyIcon.DoubleClick += delegate { ShowWindow(); };
-            _notifyIcon.Visible = true;
-        }
-
-        private void toolStripMenuItem2_Click(object sender, EventArgs e)
-        {
-            Settings.Save();
-
-            List<WallpaperAsset> assets = _assets.Where(x => x.downloadState == DownloadState.downloading).ToList();
-            if (assets.Count == 0)
-            {
-                _notifyIcon.Dispose();
-                Environment.Exit(0);
-                return;
-            }
-
-            ShowWindow();
-
-            if (MessageBox.Show(I18nWpf.GetString("LCancelDownloadConfirmTip"), Constants.ProjectName, MessageBoxButton.OKCancel) == MessageBoxResult.OK)
-            {
-                foreach (WallpaperAsset asset in assets)
-                {
-                    asset.CancelDownload();
-                }
-
-                _notifyIcon.Dispose();
-                Environment.Exit(0);
-            }
-        }
-
-        void LoadData()
-        {
-            string langJson = File.ReadAllText(Helper.GetPathForStartupFolder($@"data\{_settings.Language}.json"));
-            var langDict = TinyJson.JSONParser.FromJson<Dictionary<string, string>>(langJson);
-
-            string GetString(string key)
-            {
-                if (langDict.TryGetValue(key, out string val))
-                    return val;
-                return "";
-            }
-
-            string entriesJson = File.ReadAllText(Helper.GetPathForStartupFolder(@"data\entries.json"));
-            var model = TinyJson.JSONParser.FromJson<EntriesObject>(entriesJson);
-
-            List<WallpaperAsset> assets = new List<WallpaperAsset>();
-            List<WallpaperCategory> categories = new List<WallpaperCategory>();
-
-            foreach (var item in model.categories)
-            {
-                WallpaperCategory category = new WallpaperCategory();
-                category.title = GetString(item.localizedNameKey);
-                category.description = GetString(item.localizedDescriptionKey);
-                category.assets = new List<WallpaperAsset>();
-                foreach (var item2 in model.assets.Where(x => x.categories.Contains(item.id)))
-                {
-                    string filePath = FolderHelper.GetFilePathForURL(item2.url4KSDR240FPS, FolderHelper.DownloadPath);
-                    WallpaperAsset asset = new WallpaperAsset
-                    {
-                        id = item2.id,
-                        name = GetString(item2.localizedNameKey),
-                        previewImage = item2.previewImage,
-                        downloadURL = item2.url4KSDR240FPS,
-                    };
-                    if (File.Exists(filePath))
-                    {
-                        asset._downloadState = DownloadState.downloaded;
-                        asset.filePath = filePath;
-                    }
-                    assets.Add(asset);
-                    category.assets.Add(asset);
-                }
-                categories.Add(category);
-            }
-
-            _assets = assets;
+            List<EmojiAsset> assets = LoadData(@"C:\Users\admin\Documents\GitHub\fluentui-emoji\assets");
+            List<EmojiCategory> categories = assets.GroupBy(x => x.emoji.group)
+                .Select(x => new EmojiCategory { title = x.Key, assets = x.ToList() })
+                .ToList();
             listBox.ItemsSource = categories;
-            listBox.SelectedIndex = _settings.SelectedIndex;
-
-            WallpaperAsset selectedAsset = null;
-            string id = _settings.SelectedId;
-            if (!string.IsNullOrEmpty(id))
-            {
-                selectedAsset = _assets.Where(x => x.id == id).FirstOrDefault();
-            }
-
-            if (selectedAsset == null)
-                selectedAsset = _assets[0];
-
-            selectedAsset.isSelected = true;
-            headerGrid.DataContext = selectedAsset;
-            _lastSelectedAsset = selectedAsset;
         }
 
-        static async Task DownloadImages(List<WallpaperAsset> assets)
+        static List<EmojiAsset> LoadData(string path)
         {
-            WebClient webClient = new WebClient();
-
-            foreach (var asset in assets)
+            List<EmojiAsset> assets = new List<EmojiAsset>();
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (var dir in dirs)
             {
-                string imagePath = FolderHelper.GetFilePathForURL(asset.previewImage, FolderHelper.ImageCachePath);
-                if (!File.Exists(imagePath))
-                {
-                    try
-                    {
-                        await webClient.DownloadFileTaskAsync(asset.previewImage, imagePath);
-                    }
-                    catch
-                    {
-                        MessageBox.Show(I18nWpf.GetString("LNetworkErrorTip"), Constants.ProjectName);
+                string imageDir = Path.Combine(dir, "3D");
+                if (!Directory.Exists(imageDir))
+                    imageDir = Path.Combine(dir, @"Default\3D");
 
-                        if (File.Exists(imagePath))
-                            File.Delete(imagePath);
+                var files = Directory.GetFiles(imageDir, "*.png");
+                if (files.Length == 0)
+                    continue;
 
-                        return;
-                    }
-                }
-                asset.previewImage = imagePath;
+                string filePath = Path.Combine(dir, "metadata.json");
+                string json = File.ReadAllText(filePath);
+                EmojiObject emoji = TinyJson.JSONParser.FromJson<EmojiObject>(json);
+
+                EmojiAsset asset = new EmojiAsset();
+                asset.emoji = emoji;
+                asset.id = dir;
+                asset.previewImage = files[0];
+                asset.name = Path.GetFileName(dir);
+                assets.Add(asset);
             }
+
+            return assets;
         }
 
         private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            gridView.ItemsSource = ((WallpaperCategory)listBox.SelectedItem).assets;
+            gridView.ItemsSource = ((EmojiCategory)listBox.SelectedItem).assets;
             if (gridView.Items.Count > 0)
                 gridView.ScrollIntoView(gridView.Items[0]);
         }
 
         private void gridView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            WallpaperAsset selectedItem = (WallpaperAsset)gridView.SelectedItem;
+            EmojiAsset selectedItem = (EmojiAsset)gridView.SelectedItem;
             if (selectedItem != null)
             {
                 if (_lastSelectedAsset != null)
                     _lastSelectedAsset.isSelected = false;
 
                 selectedItem.isSelected = true;
-                headerGrid.DataContext = selectedItem;
+                header.DataContext = selectedItem;
                 _lastSelectedAsset = selectedItem;
 
-                _settings.SelectedIndex = listBox.SelectedIndex;
-                _settings.SelectedId = selectedItem.id;
+                listBox2.SelectedIndex = selectedItem.items.Count > 1 ? 1 : 0;
+                listBox2.Visibility = selectedItem.items.Count > 1 ? Visibility.Visible : Visibility.Collapsed;
             }
         }
 
-        private void ToggleLanguage(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (MessageBox.Show(I18nWpf.GetString("LRestartConfirmTip"), Constants.ProjectName, MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+            AssetItem selectedItem = (AssetItem)listBox2.SelectedItem;
+            if (selectedItem != null)
             {
-                if (_settings.Language == "en")
-                    _settings.Language = "zh_CN";
-                else
-                    _settings.Language = "en";
-
-                Settings.Save();
-
-                Process.Start(new ProcessStartInfo
-                {
-                    FileName = Process.GetCurrentProcess().MainModule.FileName,
-                    Arguments = "-r"
-                });
-
-                Environment.Exit(0);
+                Process.Start("explorer.exe", $"/select, \"{selectedItem.subitems[0].source}\"");
             }
-        }
-
-        private void ToggleAutoPlay(object sender, RoutedEventArgs e)
-        {
-            _settings.AutoPlay = toggleSwitch1.IsOn;
-            _dispatcherTimer.IsEnabled = _settings.AutoPlay;
-        }
-
-        private void ToggleStartOnBoot(object sender, RoutedEventArgs e)
-        {
-            if (toggleSwitch2.IsOn)
-                Helper.SetStartOnBoot();
-            else
-                Helper.RemoveStartOnBoot();
-        }
-
-        private void Download(object sender, RoutedEventArgs e)
-        {
-            _lastSelectedAsset.Download();
-        }
-
-        private void CancelDownload(object sender, RoutedEventArgs e)
-        {
-            _lastSelectedAsset.CancelDownload();
-        }
-
-        private void OpenFolder(object sender, RoutedEventArgs e)
-        {
-            _lastSelectedAsset.OpenFolder();
-        }
-
-        private void Preview(object sender, RoutedEventArgs e)
-        {
-            if (!Helper.IsWindows11OrGreater())
-            {
-                MessageBox.Show(I18nWpf.GetString("LNotSupportPlayTip"), Constants.ProjectName);
-                return;
-            }
-            PreviewVideo();
-
-            NativeMethods.SetCursorPos(0, 10000);
         }
     }
 }
